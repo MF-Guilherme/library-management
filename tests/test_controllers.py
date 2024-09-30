234b2f5b2851f86d7ebb69a2672d16d84582f965
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-09-30 15:49:01 -0300

    test(controller): add_book - with valid data and with empty field

---
+++ tests/test_controllers.py
@@ -0,0 +1,32 @@
+from Models.models import Book, User
+from Controllers.controllers import BookController, UserController
+import pytest
+
+
+def test_add_book_with_valid_data():
+    controller = BookController()
+    controller.add_book('Some Title', 'Some Author', '1234', 'Some Genre', '123456')
+    assert len(controller.db) == 1
+
+    added_book = controller.db[0]
+    assert added_book.title == 'Some Title'
+    assert added_book.author == 'Some Author'
+    assert added_book.year == '1234'
+    assert added_book.genre == 'Some Genre'
+    assert added_book.code == '123456'
+
+
+@pytest.mark.parametrize(
+        "title, author, year, genre, code",
+        [
+            ('', 'Some Author', '1234', 'Some Genre', '123456'), # title is empty
+            ('Some Author', '', '1234', 'Some Genre', '123456'), # author is empty
+            ('Some Author', 'Some Author', '', 'Some Genre', '123456'), # year is empty
+            ('Some Author', 'Some Author', '1234', '', '123456'), # genre is empty
+            ('Some Author', 'Some Author', '1234', 'Some Genre', '') # code is empty
+        ]
+)
+def test_add_book_raises_ValueError_if_any_field_is_empty(title, author, year, genre, code):
+    controller = BookController()
+    with pytest.raises(ValueError):
+        controller.add_book(title, author, year, genre, code)        
