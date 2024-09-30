6b9f0c0286896f5c3d90504949a433ff1f73bb74
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-09-30 18:29:58 -0300

    test(controller): Test book controller class add to create the setup_method

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,32 +1,34 @@
 from Controllers.controllers import BookController, UserController
 import pytest
 
+class TestBookController():
+    
+    def setup_method(self):
+        self.controller = BookController()
+        
+
+    def test_add_book_with_valid_data(self):
+        self.controller.add_book('Some Title', 'Some Author', '1234', 'Some Genre', '123456')
+        assert len(self.controller.db) == 1
+
+        added_book = self.controller.db[0]
+        assert added_book.title == 'Some Title'
+        assert added_book.author == 'Some Author'
+        assert added_book.year == '1234'
+        assert added_book.genre == 'Some Genre'
+        assert added_book.code == '123456'
 
 
-def test_add_book_with_valid_data():
-    controller = BookController()
-    controller.add_book('Some Title', 'Some Author', '1234', 'Some Genre', '123456')
-    assert len(controller.db) == 1
-
-    added_book = controller.db[0]
-    assert added_book.title == 'Some Title'
-    assert added_book.author == 'Some Author'
-    assert added_book.year == '1234'
-    assert added_book.genre == 'Some Genre'
-    assert added_book.code == '123456'
-
-
-@pytest.mark.parametrize(
-        "title, author, year, genre, code",
-        [
-            ('', 'Some Author', '1234', 'Some Genre', '123456'), # title is empty
-            ('Some Author', '', '1234', 'Some Genre', '123456'), # author is empty
-            ('Some Author', 'Some Author', '', 'Some Genre', '123456'), # year is empty
-            ('Some Author', 'Some Author', '1234', '', '123456'), # genre is empty
-            ('Some Author', 'Some Author', '1234', 'Some Genre', '') # code is empty
-        ]
-)
-def test_add_book_raises_ValueError_if_any_field_is_empty(title, author, year, genre, code):
-    controller = BookController()
-    with pytest.raises(ValueError):
-        controller.add_book(title, author, year, genre, code)        
+    @pytest.mark.parametrize(
+            "title, author, year, genre, code",
+            [
+                ('', 'Some Author', '1234', 'Some Genre', '123456'), # title is empty
+                ('Some Author', '', '1234', 'Some Genre', '123456'), # author is empty
+                ('Some Author', 'Some Author', '', 'Some Genre', '123456'), # year is empty
+                ('Some Author', 'Some Author', '1234', '', '123456'), # genre is empty
+                ('Some Author', 'Some Author', '1234', 'Some Genre', '') # code is empty
+            ]
+    )
+    def test_add_book_raises_ValueError_if_any_field_is_empty(self, title, author, year, genre, code):
+        with pytest.raises(ValueError):
+            self.controller.add_book(title, author, year, genre, code)        
