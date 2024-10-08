592bfbf6fa4b32c7a9fc3bbc523820c4c10f11a7
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-10-07 22:18:44 -0300

    test: delete_user returns None if user code doesn't exists

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -303,4 +303,7 @@ class TestUserController():
         assert setup_user.find_by_user_code(delete_code) in setup_user.db
         assert setup_user.delete_user(delete_code) is True
         assert setup_user.find_by_user_code(delete_code) not in setup_user.db
-        
+    
+    def test_delete_user_returns_none_if_user_code_doesnt_exists(self, setup_user):
+        delete_code = '123456'
+        assert setup_user.find_by_user_code(delete_code) is None
