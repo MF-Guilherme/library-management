93310b42eeb45ceb851c023c76cffab08f756ac5
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-10-18 21:12:55 -0300

    feat(templates): some templates created to login and register page (Drafts)

--- library_management_web/settings.py
+++ library_management_web/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'library_management_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
