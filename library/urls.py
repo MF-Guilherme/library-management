6809db62c03eec7a8c2434918697660c02bdca71
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-10-24 15:02:23 -0300

    feat(delete_book): delete_book method implemeted using Class-Based View

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
-from .views import  BookCreateView
+from .views import  BookCreateView, BookDetailView, BookDeleteView
 
 
 app_name = 'library'
@@ -9,4 +11,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('library/books/', views.get_books_info, name='books'),
     path('library/book_register/', BookCreateView.as_view(), name='book_register'),
+    path('library/book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('library/book/<int:pk>/delete/', BookDeleteView.as_view(), name='delete_book'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
