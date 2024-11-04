5cabc286523322e550fa98477d9ca15b9251b969
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-11-04 09:52:07 -0300

    feat: implementing permissions and authorizations for each page created

--- library/views.py
+++ library/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView, DetailView, DeleteView, UpdateView, ListView
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.db import transaction
@@ -11,15 +11,15 @@ from .models import Book, Loan
 from .forms import BookForm
 
 
-class BookHomeListView(ListView):
+class BookHomeListView(LoginRequiredMixin, ListView):
     model = Book    
     template_name = 'home.html'
 
 
-class BookAdminListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+class BookAdminListView(UserPassesTestMixin, ListView):
     model = Book    
     template_name = 'books.html'
-    login_url = reverse_lazy('login')  # Certifique-se de que essa URL é válida para login
+    login_url = reverse_lazy('login')
     raise_exception = False
 
     def test_func(self):
@@ -35,38 +35,74 @@ class BookAdminListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
         return redirect('/')  # Aqui redireciona para a home ou outra página de sua preferência.
 
 
-class BookCreateView(CreateView):
+class BookCreateView(UserPassesTestMixin, CreateView):
     model = Book
     form_class = BookForm
     template_name = 'book_register.html'
     success_url = reverse_lazy('library:home')
     
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+    # Redireciona para a tela de login se o usuário não estiver autenticado
+        if not self.request.user.is_authenticated:
+            messages.warning(self.request, "Por favor, faça login como administrador para acessar esta página")
+            return redirect(self.get_login_url())
+        # Lança um erro 403 apenas se o usuário não for staff
+        messages.warning(self.request, "Você não tem permissão para acessar esta página")
+        return redirect('/')  # Aqui redireciona para a home ou outra página de sua preferência.
+    
     @transaction.atomic
     def form_valid(self, form):
         messages.add_message(self.request, constants.SUCCESS, 'Livro cadastrado com sucesso!')
-        return super().form_valid(form)
+        return super().form_valid(form)    
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'book_details.html'
     context_object_name = 'book'
 
 
-class BookDeleteView(DeleteView):
+class BookDeleteView(UserPassesTestMixin, DeleteView):
     model = Book
     template_name = 'delete_book.html'
     success_url = reverse_lazy('library:books')
+
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+    # Redireciona para a tela de login se o usuário não estiver autenticado
+        if not self.request.user.is_authenticated:
+            messages.warning(self.request, "Por favor, faça login como administrador para acessar esta página")
+            return redirect(self.get_login_url())
+        # Lança um erro 403 apenas se o usuário não for staff
+        messages.warning(self.request, "Você não tem permissão para esta ação")
+        return redirect('/')  # Aqui redireciona para a home ou outra página de sua preferência.
     
     @transaction.atomic
     def form_valid(self, form):
         messages.add_message(self.request, constants.SUCCESS, 'Livro deletado com sucesso!')
         return super().form_valid(form)
     
-class BookUpdateView(UpdateView):
+class BookUpdateView(UserPassesTestMixin, UpdateView):
     model = Book
     form_class = BookForm
     template_name = 'book_update.html'
+
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+    # Redireciona para a tela de login se o usuário não estiver autenticado
+        if not self.request.user.is_authenticated:
+            messages.warning(self.request, "Por favor, faça login como administrador para acessar esta página")
+            return redirect(self.get_login_url())
+        # Lança um erro 403 apenas se o usuário não for staff
+        messages.warning(self.request, "Você não tem permissão para esta ação")
+        return redirect('/')  # Aqui redireciona para a home ou outra página de sua preferência.
 
     def get_success_url(self):
         return reverse_lazy('library:book_detail', kwargs={'pk': self.object.pk})
@@ -81,7 +117,7 @@ class BookUpdateView(UpdateView):
         messages.add_message(self.request, constants.SUCCESS, 'Livro atualizado com sucesso!')
         return super().form_valid(form)
 
-
+@login_required
 def add_to_cart(request, book_id):
     cart = request.session.get('loan_cart', [])
     if book_id not in cart:
@@ -93,11 +129,13 @@ def add_to_cart(request, book_id):
 
     return redirect('library:book_detail', pk=book_id)
 
+@login_required
 def view_cart(request):
     cart = request.session.get('loan_cart', [])
     books = Book.objects.filter(id__in=cart)
     return render(request, 'view_cart.html', {'books': books})
 
+@login_required
 def remove_from_cart(request, book_id):
     cart = request.session.get('loan_cart', [])
     if book_id in cart:
@@ -108,6 +146,7 @@ def remove_from_cart(request, book_id):
 
 from django.utils import timezone
 
+@login_required
 def finalize_loan(request):
     loan_days = 7
     cart = request.session.get('loan_cart', [])
@@ -125,7 +164,7 @@ def finalize_loan(request):
     
     return redirect('library:loans')
 
-
+@login_required
 def my_loans(request):
     loans = Loan.objects.filter(user=request.user)
     print(loans[0].book.author)
